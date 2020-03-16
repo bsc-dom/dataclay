a7606059daeb0c62fdf9bd5517052413a85a5da4
dgasull <dgasull@bsc.es>
2020-03-16 17:01:34 +0100

    Modified branch convention

--- .travis/check_branch.sh
+++ .travis/check_branch.sh
@@ -8,7 +8,7 @@ if [[ $TRAVIS_BRANCH == 'master' ]]; then
 elif [[ $TRAVIS_BRANCH == 'develop' ]]; then 
      echo "$grn OK: Branch name accomplishes branch naming conventions [develop] $end"
 else
-    valid_branch_regex="^(feature|bugfix|improvement|library|prerelease|release|hotfix)\/[a-z0-9._-]+$"
+    valid_branch_regex="^(feature|bugfix|enhancement|prerelease|release|hotfix)\/[a-z0-9._-]+$"
     if [[ ! $TRAVIS_BRANCH =~ $valid_branch_regex ]]; then
         echo "$red ERROR: There is something wrong with your branch name. Branch names in this project must adhere to this contract: $valid_branch_regex. Your PR will be rejected. You should rename your branch to a valid name and try again. $end"
         exit 1
