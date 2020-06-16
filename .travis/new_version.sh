93b9fc1d580708e2646c05e07f42e0f5aed052fa
dgasull <dgasull@bsc.es>
2020-06-16 17:01:09 +0200

    Preparing release

--- .travis/new_version.sh
+++ .travis/new_version.sh
@@ -5,13 +5,14 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
      NEW_VERSION=$(echo "$VERSION + 0.1" | bc)
      echo $NEW_VERSION > VERSION.txt
      git add VERSION.txt
-	 git commit -m "Updating version.txt"
+	 git commit -m "New release"
 	 git push origin HEAD:$TRAVIS_BRANCH
 	 
 	 ## update develop branch also ##
+	 git fetch	 
 	 git checkout develop
      git add VERSION.txt
-	 git commit -m "Updating version.txt"
+	 git commit -m "New release"
 	 git push origin HEAD:develop
 else
      echo "Skipping new version tag because current branch is not master";
