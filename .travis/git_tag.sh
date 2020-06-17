338877b69d2d9d9fb85d0e44c230d8b6f6758aa6
dgasull <dgasull@bsc.es>
2020-06-17 15:23:23 +0200

    Modified triggering of Pypi release

--- .travis/git_tag.sh
+++ .travis/git_tag.sh
@@ -2,7 +2,7 @@
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then 
      export GIT_TAG=$(cat VERSION.txt)
-     git tag $GIT_TAG -a -m "New release"
+     git tag $GIT_TAG -a -m "Generated tag from TravisCI build $TRAVIS_BUILD_NUMBER"
      git push origin $GIT_TAG
 else
      echo "Skipping Git tag because current branch is not master"
