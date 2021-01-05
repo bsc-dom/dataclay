1642201698837c5467f519d9c1855b509468258b
dgasull <dgasull@bsc.es>
2021-01-05 19:26:31 +0100

    Added encrypted keys + removed travis configs

--- .travis/git_tag.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$TRAVIS_BRANCH" == "master" ]; then 
-     export GIT_TAG=$(cat VERSION.txt)
-     git tag $GIT_TAG -a -m "Generated tag from TravisCI build $TRAVIS_BUILD_NUMBER"
-     git push origin $GIT_TAG
-else
-     echo "Skipping Git tag because current branch is not master"
-fi
