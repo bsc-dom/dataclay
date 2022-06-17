dd010fdb1665c404e2f6cb011360fbb9830401de
Marc Monfort <marc.monfort@gmail.com>
2022-06-17 17:39:12 +0200

    Add dataclay-common dependency for protos

--- src/dataclay/communication/grpc/clients/MetadataServiceGrpcClient.py
+++ src/dataclay/communication/grpc/clients/MetadataServiceGrpcClient.py
@@ -3,8 +3,8 @@ import atexit
 
 import grpc
 
-from dataclay.protos import metadata_service_pb2_grpc
-from dataclay.protos import metadata_service_pb2
+from dataclay_common.protos import metadata_service_pb2_grpc
+from dataclay_common.protos import metadata_service_pb2
 
 logger = logging.getLogger(__name__)
 
