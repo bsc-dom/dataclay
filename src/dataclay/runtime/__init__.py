e0e7019eb805e98f65fad6c5cd5baa1c7faf6dcb
Marc Monfort <marc.monfort@gmail.com>
2022-11-04 13:01:09 +0100

    Add type annotation to get_runtime()

--- src/dataclay/runtime/__init__.py
+++ src/dataclay/runtime/__init__.py
@@ -1,9 +1,14 @@
 from dataclay.runtime.settings_hub import settings, unload_settings
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from dataclay.runtime.client_runtime import ClientRuntime
+    from dataclay.runtime.execution_environment_runtime import ExecutionEnvironmentRuntime
 
 current_runtime = None
 
 
-def get_runtime():
+def get_runtime() -> "ClientRuntime | ExecutionEnvironmentRuntime":
     return current_runtime
 
 
