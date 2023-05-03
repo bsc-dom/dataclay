b646365f8b2839dfb438e93c0a0ecf3c1c7b1afa
Marc Monfort <marc.monfort@gmail.com>
2023-05-03 16:02:46 +0200

    start version 3.0.2

---
+++ CHANGES.rst
@@ -0,0 +1,10 @@
+Version 3.0.2
+-------------
+
+Unreleased
+
+
+Version 3.0.1
+-------------
+
+Released 2023-05-03
--- src/dataclay/__init__.py
+++ src/dataclay/__init__.py
@@ -4,6 +4,6 @@ Public dataclay functions exported to use (friendly) "from dataclay import ..."
 from dataclay.client.api import Client
 from dataclay.dataclay_object import DataClayObject, activemethod
 
-__version__ = "3.0.1"
+__version__ = "3.0.2.dev"
 
 StorageObject = DataClayObject
