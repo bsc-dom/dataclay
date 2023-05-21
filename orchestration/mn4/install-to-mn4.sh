3399786e77880ebe3c07edea25fd9ddb0917745a
Marc Monfort <marc.monfort@gmail.com>
2023-05-21 22:56:00 +0200

    Update orchestration install-otelcol.sh

--- orchestration/mn4/install-to-mn4.sh
+++ orchestration/mn4/install-to-mn4.sh
@@ -7,7 +7,10 @@ MN0_HOST=$BSC_USER@mn0.bsc.es
 MN_DATACLAY_PATH=/apps/DATACLAY/$DATACLAY_VERSION/
 
 # install dataClay in a Python virtual environment
-ssh $MN0_HOST "cd $MN_DATACLAY_PATH && bash -s" -- < utils/install-venv-dataclay.sh 3.10.2
+ssh $MN0_HOST "cd $MN_DATACLAY_PATH && bash -s" -- 3.10.2 <utils/install-venv-dataclay.sh
 
 # install Redis
-ssh $MN0_HOST "cd $MN_DATACLAY_PATH && bash -s" -- < utils/install-redis.sh
+ssh $MN0_HOST "cd $MN_DATACLAY_PATH && bash -s" -- <utils/install-redis.sh
+
+# install otelcol
+ssh $MN0_HOST "cd $MN_DATACLAY_PATH && bash -s" -- <utils/install-otelcol.sh
