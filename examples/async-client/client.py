e3b22e3805dff23822c925de9e5cf876037f70bd
Marc Monfort <marc.monfort@gmail.com>
2024-06-10 10:09:53 +0200

    Update async-client example

--- examples/async-client/client.py
+++ examples/async-client/client.py
@@ -1,12 +1,12 @@
 import asyncio
 
-from dataclay import AsyncClient
+from dataclay import Client
 from dataclay.contrib.modeltest.family import Dog, Family, Person
 
 
 async def main():
 
-    client = AsyncClient(host="127.0.0.1")
+    client = Client(host="127.0.0.1")
     client.start()
 
     person = Person("Marc", 24)
@@ -14,12 +14,10 @@ async def main():
 
     await person.a_make_persistent()
 
-    # TODO: Failse because it returns a coroutine for attribute access
-    # Should be fixed in the future
     print(person.is_persistent)
-    print(await person.name)
+    print(person.name)
 
-    print(await client.get_backends())
+    print(await client.a_get_backends())
 
 
 if __name__ == "__main__":
