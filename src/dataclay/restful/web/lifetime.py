c7e31612beb4b81e974cef7060ab33a113c4b2cb
Marc Monfort <marc.monfort@gmail.com>
2024-06-11 14:10:20 +0200

    Add fastapi restful module

---
+++ src/dataclay/restful/web/lifetime.py
@@ -0,0 +1,122 @@
+import logging
+from typing import Awaitable, Callable
+
+from fastapi import FastAPI
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+
+# from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+# from opentelemetry.instrumentation.logging import LoggingInstrumentor
+from opentelemetry.sdk.resources import (
+    DEPLOYMENT_ENVIRONMENT,
+    SERVICE_NAME,
+    TELEMETRY_SDK_LANGUAGE,
+    Resource,
+)
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.trace import set_tracer_provider
+
+from dataclay.restful.settings import settings
+
+
+def setup_opentelemetry(app: FastAPI) -> None:  # pragma: no cover
+    """
+    Enables opentelemetry instrumentation.
+
+    :param app: current application.
+    """
+    if not settings.opentelemetry_endpoint:
+        return
+
+    tracer_provider = TracerProvider(
+        resource=Resource(
+            attributes={
+                SERVICE_NAME: "dataclay_restful",
+                TELEMETRY_SDK_LANGUAGE: "python",
+                DEPLOYMENT_ENVIRONMENT: settings.environment,
+            },
+        ),
+    )
+
+    tracer_provider.add_span_processor(
+        BatchSpanProcessor(
+            OTLPSpanExporter(
+                endpoint=settings.opentelemetry_endpoint,
+                insecure=True,
+            ),
+        ),
+    )
+
+    excluded_endpoints = [
+        app.url_path_for("health_check"),
+        app.url_path_for("openapi"),
+        app.url_path_for("swagger_ui_html"),
+        app.url_path_for("swagger_ui_redirect"),
+        app.url_path_for("redoc_html"),
+    ]
+
+    FastAPIInstrumentor().instrument_app(
+        app,
+        tracer_provider=tracer_provider,
+        excluded_urls=",".join(excluded_endpoints),
+    )
+    LoggingInstrumentor().instrument(
+        tracer_provider=tracer_provider,
+        set_logging_format=True,
+        log_level=logging.getLevelName(settings.log_level.value),
+    )
+
+    set_tracer_provider(tracer_provider=tracer_provider)
+
+
+def stop_opentelemetry(app: FastAPI) -> None:  # pragma: no cover
+    """
+    Disables opentelemetry instrumentation.
+
+    :param app: current application.
+    """
+    if not settings.opentelemetry_endpoint:
+        return
+
+    FastAPIInstrumentor().uninstrument_app(app)
+
+
+def register_startup_event(
+    app: FastAPI,
+) -> Callable[[], Awaitable[None]]:  # pragma: no cover
+    """
+    Actions to run on application startup.
+
+    This function uses fastAPI app to store data
+    in the state, such as db_engine.
+
+    :param app: the fastAPI application.
+    :return: function that actually performs actions.
+    """
+
+    @app.on_event("startup")
+    async def _startup() -> None:  # noqa: WPS430
+        app.middleware_stack = None
+        setup_opentelemetry(app)
+        app.middleware_stack = app.build_middleware_stack()
+        pass  # noqa: WPS420
+
+    return _startup
+
+
+def register_shutdown_event(
+    app: FastAPI,
+) -> Callable[[], Awaitable[None]]:  # pragma: no cover
+    """
+    Actions to run on application's shutdown.
+
+    :param app: fastAPI application.
+    :return: function that actually performs actions.
+    """
+
+    @app.on_event("shutdown")
+    async def _shutdown() -> None:  # noqa: WPS430
+        stop_opentelemetry(app)
+        pass  # noqa: WPS420
+
+    return _shutdown
