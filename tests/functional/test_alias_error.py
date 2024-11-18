2a39ff3c04fd2905a5c1fda06b5c881c4994ff7f
Canicula98 <david.canocanardo@bsc.es>
2024-11-18 18:52:37 +0100

    exception test fix

--- tests/functional/test_alias_error.py
+++ tests/functional/test_alias_error.py
@@ -1,16 +1,21 @@
 import pytest
 
 from dataclay.contrib.modeltest.family import Person
-from dataclay.exceptions import DataClayException, ObjectNotRegisteredError
+from dataclay.exceptions import (
+    AliasDoesNotExistError,
+    AlreadyExistError,
+    DataClayException,
+    ObjectNotRegisteredError,
+)
 
 
 def test_alias_already_exists(client):
     person_1 = Person("Marc", 24)
     person_2 = Person("Alice", 21)
     person_1.make_persistent("test_same_alias_error")
-    with pytest.raises(DataClayException) as excinfo:
+    with pytest.raises(AlreadyExistError) as excinfo:
         person_2.make_persistent("test_same_alias_error")
-    assert "Alias admin/test_same_alias_error already exist" in str(excinfo.value)
+    assert "admin/test_same_alias_error already exists" in str(excinfo.value)
 
 
 @pytest.mark.asyncio
@@ -18,22 +23,22 @@ async def test_alias_already_exists_async(client):
     person_1 = Person("Marc", 24)
     person_2 = Person("Alice", 21)
     await person_1.a_make_persistent("test_same_alias_error_async")
-    with pytest.raises(DataClayException) as excinfo:
+    with pytest.raises(AlreadyExistError) as excinfo:
         await person_2.a_make_persistent("test_same_alias_error_async")
-    assert "Alias admin/test_same_alias_error_async already exist" in str(excinfo.value)
+    assert "admin/test_same_alias_error_async already exists" in str(excinfo.value)
 
 
 def test_alias_does_not_exist(client):
-    with pytest.raises(DataClayException) as excinfo:
+    with pytest.raises(AliasDoesNotExistError) as excinfo:
         Person.get_by_alias("test_alias_does_not_exist_error")
-    assert "Alias None/test_alias_does_not_exist_error does not exists" in str(excinfo.value)
+    assert "Alias None/test_alias_does_not_exist_error does not exist" in str(excinfo.value)
 
 
 @pytest.mark.asyncio
 async def test_alias_does_not_exist_async(client):
-    with pytest.raises(DataClayException) as excinfo:
+    with pytest.raises(AliasDoesNotExistError) as excinfo:
         await Person.a_get_by_alias("test_alias_does_not_exist_error_async")
-    assert "Alias None/test_alias_does_not_exist_error_async does not exists" in str(excinfo.value)
+    assert "Alias None/test_alias_does_not_exist_error_async does not exist" in str(excinfo.value)
 
 
 def test_add_alias_notRegistered(client):
