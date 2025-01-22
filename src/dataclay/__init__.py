db8fffa202cdb59cf51e23e7a517a14cc6f56688
Alex Barcelo <alex@betarho.net>
2025-01-22 17:57:37 +0100

    initial implementation of StubDataClayObject

--- src/dataclay/__init__.py
+++ src/dataclay/__init__.py
@@ -4,10 +4,10 @@ Public dataclay functions exported to use (friendly) "from dataclay import ..."
 
 from dataclay.client.api import Client
 from dataclay.dataclay_object import DataClayObject, activemethod
-
-from dataclay.alien import AlienDataClayObject  # isort: skip
+from dataclay.alien import AlienDataClayObject
+from dataclay.stub import StubDataClayObject
 
 StorageObject = DataClayObject
 
 __version__ = "4.2.0-rc"
-__all__ = ["Client", "DataClayObject", "AlienDataClayObject", "activemethod", "StorageObject"]
+__all__ = ["Client", "DataClayObject", "AlienDataClayObject", "StubDataClayObject", "activemethod", "StorageObject"]
