72cc393bfd8fb122c15c8f6141a23dcb4082d5b8
Alex Barcelo <alex@betarho.net>
2025-02-12 16:18:14 +0100

    minimal seemingly working approach to dynamic dependencies for legacydeps

--- compile_protos.py
+++ compile_protos.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
+import inspect
+from typing import Any
 from importlib import resources
 from typing import Any
-import grpc_tools.protoc
 
 try:
     from hatchling.builders.hooks.plugin.interface import BuildHookInterface
@@ -14,6 +15,10 @@ except ModuleNotFoundError:
 
 
 def run_protoc():
+    # Here because during the build process, CustomBuildHook will be imported
+    # *before* knowing the dependencies of the hook itself.
+    import grpc_tools.protoc
+
     grpc_tools_proto = (resources.files("grpc_tools") / "_proto").resolve()
     grpc_tools.protoc.main(
         [
@@ -29,12 +34,38 @@ def run_protoc():
     )
 
 
+def find_config_settings_in_hatchling() -> dict[str, Any]:
+    # Terrible workaround (their words, not mine) given by @virtuald
+    # https://github.com/pypa/hatch/issues/1072#issuecomment-2448985229
+    # Hopefully this will be fixed in the future
+    for frame_info in inspect.stack():
+        frame = frame_info.frame
+        module = inspect.getmodule(frame)
+        if (
+            module
+            and module.__name__.startswith("hatchling.build")
+            and "config_settings" in frame.f_locals
+        ):
+            return frame.f_locals["config_settings"]
+
+    return {}
+
+
 class CustomBuildHook(BuildHookInterface):
     def initialize(self, version: str, build_data: dict[str, Any]) -> None:
         run_protoc()
 
     def dependencies(self):
-        return ["grpcio-tools==1.48.2"]
+        if find_config_settings_in_hatchling().get("LEGACY_DEPS", "False").lower() in (
+            "true",
+            "on",
+            "1",
+            "y",
+            "yes",
+        ):
+            return ["grpcio-tools==1.48.2"]
+        else:
+            return ["grpcio-tools==1.67.1"]
 
 
 if __name__ == "__main__":
