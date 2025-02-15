3ccbf1f07b4db0d1e31c57107de3a55d5fdaab4f
Marc Monfort <marc.monfort@gmail.com>
2025-02-15 17:17:54 +0800

    Move AlienDataClayObject import to the correct position in __init__.py to avoid cyclic dependencies

--- src/dataclay/__init__.py
+++ src/dataclay/__init__.py
@@ -4,8 +4,9 @@ Public dataclay functions exported to use (friendly) "from dataclay import ..."
 
 from dataclay.client.api import Client
 from dataclay.dataclay_object import DataClayObject, activemethod
-from dataclay.alien import AlienDataClayObject
 from dataclay.stub import StubDataClayObject
+
+from dataclay.alien import AlienDataClayObject  # isort: skip
 
 StorageObject = DataClayObject
 
